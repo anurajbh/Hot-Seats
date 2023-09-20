454773b5419628c38805323016ba2fc1513dface
anurajbh <42970022+anurajbh@users.noreply.github.com>
2023-09-20 19:16:46 +0530

    simple 2d movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //stats
+    [SerializeField] float movementSpeed;
+    public Color playerColor;
+    public int playerScore;
+    public bool isSitting = false;
+    Rigidbody2D playerRigidbody2D;
+    // Update is called once per frame
+    private void Awake()
     {
-        
+        playerRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        //movement method
+        MoveThePlayer();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MoveThePlayer()
     {
-        
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticallInput = Input.GetAxisRaw("Vertical");
+        playerRigidbody2D.velocity = new Vector2(horizontalInput * movementSpeed, verticallInput * movementSpeed);
     }
 }
